17e61df56090585e4dde25a4abf5776813abe06c
Veroski <guillemvera@gmail.com>
2023-05-06 15:35:12 +0200

    misRutinas actualisao un poquillo

--- app/src/main/java/com/example/chymv2/view/ActivityMisRutinas.java
+++ app/src/main/java/com/example/chymv2/view/ActivityMisRutinas.java
@@ -2,6 +2,9 @@ package com.example.chymv2.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,20 +13,28 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.chymv2.R;
+import com.example.chymv2.adapters.RoutineListAdapter;
+import com.example.chymv2.model.Rutina;
+import com.example.chymv2.sources.InitializeData;
+import com.example.chymv2.viewmodel.RoutineViewModel;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class ActivityMisRutinas extends AppCompatActivity {
-
+public class ActivityMisRutinas extends AppCompatActivity implements SearchView.OnQueryTextListener{
+    private RecyclerView recyclerView;
+    private RoutineListAdapter routineListAdapter;
+    private RoutineViewModel routineViewModel;
+    private androidx.appcompat.widget.SearchView routineSearch;
+    private static InitializeData data;
     private Button returnMain_rutinasRecomendadas_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rutinas_recomendadas);
+        setContentView(R.layout.activity_mis_rutinas);
 
-        NavigationBarView navigation = findViewById(R.id.bottom_navigation_rutinasRecomendadas);
+        NavigationBarView navigation = findViewById(R.id.bottom_navigation_misRutinas);
         navigation.setOnItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        returnMain_rutinasRecomendadas_btn = findViewById(R.id.returnMain_rutinasRecomendadas_btn);
+        returnMain_rutinasRecomendadas_btn = findViewById(R.id.returnMain_misRutinas_btn);
 
         returnMain_rutinasRecomendadas_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +44,10 @@ public class ActivityMisRutinas extends AppCompatActivity {
         });
         //assert getSupportActionBar() != null;
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        data = new InitializeData(this);
+        routineViewModel = new RoutineViewModel(this);
+        initlistaRutinas();
+        initListenerRoutines();
     }
     /*@Override
     public boolean onSupportNavigateUp() {
@@ -60,5 +75,39 @@ public class ActivityMisRutinas extends AppCompatActivity {
             return false;
         }
     };
+    private void initlistaRutinas() {
+        routineListAdapter = new RoutineListAdapter(routineViewModel.getRoutines().getValue(),this, new RoutineListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Rutina item) {
+                moveToRoutine(item);
+            }
+        });
+        routineListAdapter.notifyDataSetChanged();
+        routineSearch = findViewById(R.id.routineSearch);
 
+        recyclerView = findViewById(R.id.routineListRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(routineListAdapter);
+    }
+    private void initListenerRoutines(){
+        routineSearch.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        routineListAdapter.filter(s);
+        routineListAdapter.notifyDataSetChanged();
+        return false;
+    }
+    public void moveToRoutine(Rutina item){
+        Intent intent = new Intent(this, ActivityRoutineDescription.class);
+        intent.putExtra("Rutina", item);
+        startActivity(intent);
+    }
 }
